ffd4a10250712579c964a1ce3c7016aac17e5ea8
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2021-08-27 13:34:52 +0300

    graph functionality

--- happ.py
+++ happ.py
@@ -1,10 +1,12 @@
 import sys, mood_db
+import matplotlib.pyplot as plt
 # from pathlib import Path
 # from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QStatusBar
 # from PyQt5.QtGui import QIcon
 from interface import Ui_MainWindow
 
+#TODO: Change button "Show Chart" to "Show Graph"
 #BUG: Clicking on anywhere on the slider makes it go to either 0 or 10
 #BUG: Slider is scrollable with mouse scroll. Also, scrolling with mouse does not invoke sliderMoved, so the label does not update the text
 
@@ -33,7 +35,7 @@ class Application(QMainWindow):
         self.ui.btn_save.clicked.connect(self.save_to_db)
 
         # Create a chart from saved data
-        self.ui.btn_chart.clicked.connect(self.show_chart)
+        self.ui.btn_chart.clicked.connect(self.show_graph)
 
     def label_update(self):
         self.ui.lbl_mood.setText(f"Rate Your Mood - {self.ui.slider_mood.sliderPosition()}")
@@ -54,17 +56,27 @@ class Application(QMainWindow):
             )
         if save == 0:
             # TODO: If a rating was already entered for the current day, show warning box with OK | Cancel that it will override the existing entry.
-            pass
+            print("Save was unsuccessful because an entry for today already exists!")
+            print("Would you like to override it? \033[1;31;40m[y/n]\033[0;37;40m")
         else:
             # PROMPT, Data entered succesfully. The data you entered:
-            pass
+            print("Data entry successful")
 
-    def show_chart(self):
+    def show_graph(self):
         # TODO: if not mood_db.show_values():pass ; else: create a for loop for a chart with retrieved values from database
-        print("\033[1;37;40mCurrent values in database\033[0;37;40m")
-        for each in mood_db.show_values():
-            print(each)
-        # print()
+        values = mood_db.show_values()
+        if not values:
+            print("There are currently no mood ratings saved in database.")
+        else:
+            print("\033[1;37;40mCurrent values in database:\033[0;37;40m")
+            x = [x[-1] for x in values]
+            y = [y[0] for y in values]
+            print("x:", x)
+            print("y:", y)
+            plt.plot(x, y)
+            plt.show()
+            # for each in values:
+            #     print(each)
 
 def app_window():
     app = QApplication(sys.argv)
